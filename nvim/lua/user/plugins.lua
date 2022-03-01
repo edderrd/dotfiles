5d65ff856fabac1e6e34ebc7c376f6cc8c980456
Edder Rojas <edder.rojas@gmail.com>
2022-03-01 17:55:14 -0600

    added glow (markdown preview) plugin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -109,6 +109,8 @@ return packer.startup(function(use)
 		end,
 	})
 
+	use("ellisonleao/glow.nvim")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
