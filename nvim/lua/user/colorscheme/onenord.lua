f421f8560e53f83479a7d39bca5ede4a409a4e9f
Edder Rojas <edder.rojas@gmail.com>
2022-05-10 12:34:39 -0600

    nvim: fixed telescope highlight

--- nvim/lua/user/colorscheme/onenord.lua
+++ nvim/lua/user/colorscheme/onenord.lua
@@ -1,11 +1,17 @@
+local colors = require("onenord.colors").load()
+
 local M = {
 	borders = true,
+	fade_nc = true,
 	styles = {
 		diagnostics = "undercurl",
 		comments = "italic",
 		keywords = "italic",
 		functions = "italic,bold",
 		strings = "italic",
+	},
+	custom_highlights = {
+		TelescopeSelection = { bg = colors.gray, fg = colors.fg_light },
 	},
 	custom_colors = {
 		-- bg = "#282c34",
