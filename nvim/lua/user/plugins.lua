7663ece385f5c8d8d98a40893a62dcbb94c75a6d
Edder Rojas <edder.rojas@gmail.com>
2022-06-08 11:17:59 -0600

    nvim: better plugin for tmux with clipboard support

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -49,7 +49,29 @@ return packer.startup(function(use)
 	use("navarasu/onedark.nvim")
 	use("rmehri01/onenord.nvim")
 
-	use("christoomey/vim-tmux-navigator") -- Tmux navigation <control>arrows
+	use({
+		"aserowy/tmux.nvim",
+		config = function()
+			require("tmux").setup({
+				-- overwrite default configuration
+				-- here, e.g. to enable default bindings
+				copy_sync = {
+					-- enables copy sync and overwrites all register actions to
+					-- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
+					enable = true,
+					redirect_to_clipboard = true,
+				},
+				navigation = {
+					-- enables default keybindings (C-hjkl) for normal mode
+					enable_default_keybindings = true,
+				},
+				resize = {
+					-- enables default keybindings (A-hjkl) for normal mode
+					enable_default_keybindings = true,
+				},
+			})
+		end,
+	})
 
 	use("kyazdani42/nvim-tree.lua") -- File explorer
 	use("nvim-telescope/telescope.nvim") -- very fast and intuitive file searcher
