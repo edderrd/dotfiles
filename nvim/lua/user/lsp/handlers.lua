dc024b77a9044e0c54940482b364016bb39c2380
Edder Rojas <edder.rojas@gmail.com>
2022-08-18 19:45:03 -0600

    nvim: disable volar formatting

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -121,6 +121,10 @@ M.on_attach = function(client, bufnr)
 		client.resolved_capabilities.document_range_formatting = false
 	end
 
+	if client.name == "volar" then
+		client.resolved_capabilities.document_formatting = false
+	end
+
 	lsp_keymaps(bufnr)
 	lsp_highlight_document(client)
 end
