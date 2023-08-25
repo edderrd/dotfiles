f3c594536657ae35074edc4061d939fe778d0e14
Edder Rojas <edder.rojas@gmail.com>
2023-08-25 16:36:03 -0600

    allow fnm to override nvm

--- zsh/fnm.zsh
+++ zsh/fnm.zsh
@@ -1,4 +1,12 @@
 if (( $+commands[fnm] )); then
+  # override nvm
+  function nvm() {
+    YELLOW='\033[0;33m'
+    NC='\033[0m' # No Color
+    echo -e "${YELLOW}WARNING: Using fnm${NC}\n"
+    fnm $@
+  }
+
   eval "$(fnm env --use-on-cd)"
   eval "$(fnm completions --shell zsh)"
 fi
