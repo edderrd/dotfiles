d3942ec2c6a3a26e4db85260501d39315b3099d3
Edder Rojas <edder.rojas@gmail.com>
2024-05-08 09:06:33 -0600

    feat[wezterm]: support for better color scheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,20 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("ComicCodeLigatures NF"),
+	font = wezterm.font("ComicCodeLigatures NFM"),
 	font_size = 13,
 	line_height = 1,
 	enable_tab_bar = false,
-	color_scheme = "OneDark (Gogh)",
+	-- color_scheme = "OneDark (Gogh)",
+	color_scheme = "Catppuccin Mocha",
 	--[[ term = "wezterm", ]]
 	-- window_decorations = " TITLE | RESIZE",
 	window_decorations = "RESIZE",
 	--[[ window_background_opacity = 0.9, ]]
 	window_padding = {
-		left = 2,
-		right = 2,
-		bottom = 2,
-		top = 2,
+		left = 8,
+		right = 8,
+		bottom = 8,
+		top = 8,
 	},
 }
