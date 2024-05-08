fd1151e2785bfeafb685e2d0dc1513221ecf5a90
Edder Rojas <edder.rojas@gmail.com>
2024-05-08 09:06:51 -0600

    feat[zsh]: added better history management

--- zsh/nix.zsh
+++ zsh/nix.zsh
@@ -161,3 +161,12 @@ if (( $+commands[fuck] )); then
     eval $(thefuck --alias fk)
 fi
 
+## history setup
+HISTFILE=$HOME/.zhistory
+SAVEHIST=1000
+HISTSIZE=999
+setopt share_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_verify
+
