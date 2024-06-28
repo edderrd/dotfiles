138a47669a6ff2ef8799e4047d075bfa4bd2d038
Edder Rojas <edder.rojas@gmail.com>
2024-06-28 11:08:09 -0600

    feat(wezterm): better configuration

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,20 +1,48 @@
 local wezterm = require("wezterm")
+local config = {}
 
-return {
-	font = wezterm.font("ComicCodeLigatures NFM"),
-	font_size = 13,
-	line_height = 1,
-	enable_tab_bar = false,
-	-- color_scheme = "OneDark (Gogh)",
-	color_scheme = "Catppuccin Mocha",
-	--[[ term = "wezterm", ]]
-	-- window_decorations = " TITLE | RESIZE",
-	window_decorations = "RESIZE",
-	--[[ window_background_opacity = 0.9, ]]
-	window_padding = {
-		left = 8,
-		right = 8,
-		bottom = 8,
-		top = 8,
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.use_dead_keys = false
+config.font = wezterm.font("ComicCodeLigatures NF")
+config.font_size = 13
+config.line_height = 1.2
+config.enable_tab_bar = false
+config.underline_position = -7
+config.underline_thickness = 2
+config.front_end = "WebGpu"
+config.term = "wezterm"
+config.color_scheme = "Catppuccin Mocha"
+config.window_decorations = "RESIZE | MACOS_FORCE_ENABLE_SHADOW"
+-- config.window_decorations = " TITLE | RESIZE"
+-- config.window_background_opacity = 0.8
+-- config.macos_window_background_blur = 20
+config.window_padding = {
+	left = 20,
+	right = 20,
+	bottom = 20,
+	top = 20,
+}
+
+config.keys = {
+	-- Make Option-Left and Option-Right not retarded on macOS. Now that key combo jumps word left or right
+	--
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+	--
+}
+
+config.mouse_bindings = {
+	-- Ctrl-click will open the link under the mouse cursor
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.OpenLinkAtMouseCursor,
 	},
 }
+
+return config
