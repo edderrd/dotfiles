56e156d95ea88f15162462379400cf48aa3f5468
Edder Rojas <edder.rojas@gmail.com>
2024-09-12 09:29:49 -0600

    feat(nvim): allow to move lines up/down visual mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,3 +4,6 @@
 vim.keymap.set("n", ";w", ":w!<cr>", { silent = true, noremap = true, desc = "Quick Save" })
 vim.keymap.set("n", ";qa", ":qa!<cr>", { silent = true, noremap = true, desc = "Quit All" })
 vim.keymap.set("n", ";x", ":x!<cr>", { silent = true, noremap = true, desc = "Save and Quit" })
+
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", { silent = true, noremap = true, desc = "Move line up" }) -- move line up(v)
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", { silent = true, noremap = true, desc = "Move line down" }) -- move line down(v)
