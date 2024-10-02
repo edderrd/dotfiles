8bb1787a982d5e7f9bc2933f275ae65873f39cf9
Edder Rojas <edder.rojas@gmail.com>
2024-10-02 15:52:08 -0600

    fix(nvim): allow clipboard to work on linux

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,10 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- relative numbers
 vim.opt.relativenumber = true
 -- vim.opt.winbar = "%=%m %f"
 
+-- allow anywhere clipboard support
+vim.opt.clipboard = "unnamedplus"
+
+-- disable popup transparencies
 vim.opt.pumblend = 0
 vim.opt.winblend = 0
 
+-- use intelephense for php
 vim.g.lazyvim_php_lsp = "intelephense"
