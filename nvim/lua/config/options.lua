16bbc77b3b787e50d231f88b72dc5f7a69b0d3fb
Edder Rojas <edder.rojas@gmail.com>
2025-01-21 14:46:05 -0600

    chore(nvim): packages updates and cleanpup

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,8 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- relative numbers
-vim.opt.relativenumber = true
 -- vim.opt.winbar = "%=%m %f"
 
 -- allow anywhere clipboard support
