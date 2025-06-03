153ff863a4cccd32390620471377969a6d7abc52
Edder Rojas <edder.rojas@gmail.com>
2025-06-02 19:19:32 -0600

    chore(nvim): update packages

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy",
   opts = {
     presets = {
-      bottom_search = false,
+      bottom_search = true,
       command_palette = false,
       long_message_to_split = true,
     },
